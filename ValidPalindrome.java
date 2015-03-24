ee5335851d979c101c3792f31ccae6b1c7d2a996
taihangy <yetaihang.zju@gmail.com>
2015-03-24 23:57:13 +0800

    j>= is important

--- ValidPalindrome.java
+++ ValidPalindrome.java
@@ -4,7 +4,7 @@ public class ValidPalindrome{
 		if(s.length()==0) return true;
 		s=s.toLowerCase();
 		s=s.replaceAll("[^0-9a-z]+","");
-		for(int i=0,j=s.length()-1;i<s.length()/2&&j>s.length()/2;i++,j--){
+		for(int i=0,j=s.length()-1;i<s.length()/2&&j>=s.length()/2;i++,j--){
 			if(s.charAt(i)!=s.charAt(j)) return false;
 		}
 		return true;
