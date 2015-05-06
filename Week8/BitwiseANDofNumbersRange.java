e5786d3807e05b8f77bd56fce61b11d4617fc8c6
taihangy <yetaihang.zju@gmail.com>
2015-05-06 23:42:58 +0800

    Week8 Day3

---
+++ Week8/BitwiseANDofNumbersRange.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        int mask=Integer.MAX_VALUE;
+        while((m&mask)!=(n&mask)) mask=mask<<1;
+        return n&mask;
+    }
+}
