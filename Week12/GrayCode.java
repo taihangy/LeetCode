64009886c5532fd286e14dddfc8bd6cc1cdd4840
taihangy <yetaihang.zju@gmail.com>
2015-06-06 23:45:56 +0800

    Week12 Day3

---
+++ Week12/GrayCode.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> res=new ArrayList<Integer>();
+        if(n<0) return res;
+        if(n==0){
+            res.add(0);
+            return res;
+        }
+        res.add(0);
+        res.add(1);
+        for(int i=2;i<=n;i++){
+            int size=res.size();
+            for(int j=size-1;j>=0;j--){
+                res.add(res.get(j)+(1<<(i-1)));
+            }
+        }
+        return res;
+    }
+}
