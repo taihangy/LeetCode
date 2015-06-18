b56fba6ccd41eb37f6e93d1043a6ddf7f6f5979e
taihangy <yetaihang.zju@gmail.com>
2015-06-18 12:04:48 +0800

    Week12

--- Week12-17/MaximalSquare.java
+++ Week12-17/MaximalSquare.java
@@ -31,4 +31,31 @@ public class Solution {
         }
         return max;
     }
+
+    //DP
+    public int maximalSquare(char[][] matrix) {
+        if(matrix==null||matrix.length==0||matrix[0].length==0) return 0;
+        int max=0;
+        for(int i=0;i<matrix.length;i++)
+            if(matrix[i][0]=='1') {
+                max=1;
+                break;
+            }
+        if(max==0){
+            for(int i=0;i<matrix[0].length;i++)
+                if(matrix[0][i]=='1') {
+                    max=1;
+                    break;
+                }
+        }
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[0].length;j++){
+                if(matrix[i][j]=='1'){
+                    matrix[i][j]=(char)((Math.min(Math.min(matrix[i][j-1]-'0',matrix[i-1][j]-'0'),matrix[i-1][j-1]-'0')+1)+'0');
+                    max=Math.max(max,matrix[i][j]-'0');
+                }
+            }
+        }
+        return max*max;
+    }
 }
