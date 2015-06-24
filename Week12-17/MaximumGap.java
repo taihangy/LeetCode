962875b8395a5483640b1b12eeee47b243253b5d
taihangy <yetaihang.zju@gmail.com>
2015-06-24 15:35:29 +0800

    Week12

--- Week12-17/MaximumGap.java
+++ Week12-17/MaximumGap.java
@@ -31,4 +31,41 @@ public class Solution {
         }
         return res;
     }
+
+    public int maximumGap(int[] nums) {
+        if(nums==null||nums.length<2) return 0;
+        sort(nums,0,nums.length-1,1);
+        int max=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]-nums[i-1]>max) max=nums[i]-nums[i-1];
+        }
+        return max;
+    }
+    
+    private static void sort(int[] nums,int lo,int hi,int d){
+        if(hi<=lo||d>4) return;
+        int lt=lo,gt=hi;
+        int v=numAtD(nums[lo],d);
+        int i=lo+1;
+        while(i<=gt){
+            int t=numAtD(nums[i],d);
+            if(t>v) exch(nums,i,gt--);
+            else if(t<v) exch(nums,i++,lt++);
+            else i++;
+        }
+        sort(nums,lo,lt-1,d);
+        sort(nums,lt,gt,d+1);
+        sort(nums,gt+1,hi,d);
+    }
+    
+    private static void exch(int[] nums, int i, int j) {
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+
+    private static int numAtD(int num,int d){
+        int mask=(((1<<31)>>31)>>>24)<<((4-d)*8);
+        return num&mask;
+    }
 }
